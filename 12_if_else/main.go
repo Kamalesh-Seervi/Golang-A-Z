a7ce22a1d32e160196d4d1ec02bdced683a875ba
kd14 <kd14@ford.com>
2023-07-31 21:49:58 +0530

    Old repo deleted

---
+++ 12_if_else/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("IF else in GoLang")
+
+	//use open curly brace just after the if condition dont put in next line it will throw error becuase of lexers
+	num := 35
+	var result string
+	if num > 50 {
+		result = "Good"
+	} else {
+		result = "Bad"
+	}
+	fmt.Println(result)
+
+	// another way if useful when you use to create sites
+	// get user imput in if directly
+
+	if nums := 3; nums < 10 {
+		fmt.Println("Good")
+	} else {
+		fmt.Println("Bad")
+	}
+
+}
